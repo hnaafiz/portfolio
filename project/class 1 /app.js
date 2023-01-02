a9c95c729ba76d87d333c337880983451da363aa
hnaafiz <121534365+hnaafiz@users.noreply.github.com>
2023-01-02 22:03:45 +0500

    beta

---
+++ project/class 1 /app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const btnMore= document.querySelector('.show-more');
+const readMore = document.querySelector('.readMore');
+const btnless = document.querySelector('.show-less')
+btnMore.addEventListener('click', function () {
+   
+    btnMore.addEventListener('click', function() {
+
+
+
+        readMore.classList.remove('readMore')
+        btnMore.className = 'btn-hide';
+        btnless.classList.remove('show-less');
+
+
+    })
+
+   
+
+
+    
+
+   
+    
+
+})
+
